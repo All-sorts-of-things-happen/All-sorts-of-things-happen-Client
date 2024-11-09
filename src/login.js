7ebf6e94753ce1fb7940b28d3462c1fd89796754
정상미 <w2321@e-mirim.hs.kr>
2024-11-10 02:27:10 +0900

    react 익혀보기

---
+++ src/login.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Login = () => {
+    return (
+        <div>
+            <h1>내 컴포넌트</h1>
+            <p>이거머지..</p>
+        </div>
+    );
+};
+
+export default Login;
